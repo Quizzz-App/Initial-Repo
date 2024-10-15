74b6555375b485c4df050358103a97d45f661bac
MiniSkeTOriG <skirtm64@gmail.com>
2024-10-15 00:50:21 +0000

    update

--- frontend/javascripts/auxilliarypagejs/quizpage.js
+++ frontend/javascripts/auxilliarypagejs/quizpage.js
@@ -1,1 +1,41 @@
 
+const stylecolor = getComputedStyle(body);
+const progressRing = document.querySelector('.progress-ring circle');
+const progressText = document.getElementById('progress-text');
+
+var progressstart=0;
+var currentprogress=0;
+
+var calctime=1*10;
+
+function updateProgress() {
+   currentprogress=((progressstart/calctime)*100);
+ 
+  progressText.textContent = `${convertSecondsToTime(progressstart)}`;
+  progressRing.style.strokeDashoffset = 0 + ((currentprogress/100) * 630);
+   
+  progressstart++;
+  
+  if (progressstart <= calctime) {
+    setTimeout(updateProgress, 1000);
+  }else{
+     console.log("complete");
+     progressRing.style.fill = stylecolor.getPropertyValue('--primitivecolor');
+     progressText.textContent = "Time Up";
+     progressText.style.color = stylecolor.getPropertyValue('--primarycolor');
+  }
+
+}
+
+updateProgress();
+
+function convertSecondsToTime(seconds) {
+   let minutes = Math.floor((seconds % 3600) / 60);
+   let secs = seconds % 60;
+
+   // Pad the hours, minutes, and seconds with leading zeros if needed
+   minutes = String(minutes).padStart(2, '0');
+   secs = String(secs).padStart(2, '0');
+
+   return `${minutes}:${secs}`;
+}
