44cbf75089063351f4d55803c2c3a6f1db7d466e
MiniSkeTOriG <skirtm64@gmail.com>
2024-11-01 07:42:10 +0000

    new update

--- frontend/javascripts/adminpagejs/usermanagement.js
+++ frontend/javascripts/adminpagejs/usermanagement.js
@@ -3,6 +3,10 @@ const usermetview = [...document.getElementsByClassName("usermetview")];
 const usermetholder = document.querySelector(".usermetholder");
 const usermetpanelclose = document.getElementById("usermetpanelclose");
 
+const deactivatereqview = [...document.getElementsByClassName("deactivatereqview")];
+const deactivationreqholder = document.querySelector(".deactivationreqholder");
+const deactivationreqpanelclose = document.getElementById("deactivationreqpanelclose");
+
 usermetview.forEach((usermet,i)=>{
    usermet.addEventListener('click',()=>{
       usermetholder.classList.toggle("active",true);
@@ -11,5 +15,16 @@ usermetview.forEach((usermet,i)=>{
 })
 usermetpanelclose.addEventListener('click',()=>{
    usermetholder.classList.toggle("active",false);
+   body.style.overflowY="auto";
+})
+
+deactivatereqview.forEach((reqdetail,i)=>{
+   reqdetail.addEventListener('click',()=>{
+      deactivationreqholder.classList.toggle("active",true);
+      body.style.overflowY="hidden";
+   })
+})
+deactivationreqpanelclose.addEventListener('click',()=>{
+   deactivationreqholder.classList.toggle("active",false);
    body.style.overflowY="auto";
 })
