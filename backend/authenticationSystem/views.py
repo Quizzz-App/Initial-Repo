56bc4ee6c91899b89689b2add52546cb13af7c31
qwabena-proxy <qwabenamike91@gmail.com>
2024-11-08 17:41:56 -0500

    Update

--- backend/authenticationSystem/views.py
+++ backend/authenticationSystem/views.py
@@ -390,45 +390,35 @@ def logout_page(request, *args, **kwargs):
 
 def passwordReset(request):
     messages_to_display= messages.get_messages(request)
-    form= PasswordResetRequestForm()
     if request.method == 'POST':
-        form= PasswordResetRequestForm(request.POST)
-        if form.is_valid():
-            data= form.cleaned_data.get('email')
-            try:
-                user= User.objects.get(email= data)
-            except (TypeError, ValidationError, User.DoesNotExist):
-                user= None
+        requestEmail= request.POST.get('email')
+        try:
+            user= User.objects.get(email= requestEmail)
+        except (TypeError, ValidationError, User.DoesNotExist):
+            user= None
 
-            if user is not None:
-                token= TokenGeneratorValidator.make_token(user) #Generating hash 
-                tokenID= TokensModel.objects.create(token=token, user_id=user.id) # Registering token to database
-                current_site= get_current_site(request) #Geting the curent site domain
-                mail_subject= 'Password Reset' #Email to be sent preparation process
-                message= render_to_string('auth/mail/passwordResetMail.html', {
-                    'user': user,
-                    'domain': current_site,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'token': token
-                })
-                to_email= form.cleaned_data.get('email')
-                email= EmailMessage(
-                mail_subject, message, to=[to_email]
-            )
-                email.send()
-                tokenID.save()
-                messages.success(request, 'Please check your email to reset your password..')
-                return redirect('index')
-            
-            
-            else:
-                messages.error(request, 'No account was found with the provided E-mail. Please check and try again...')
-                return redirect('password-reset-request')
-    return render(request, 'auth/passwordReset.html', context={
-        'form': PasswordResetRequestForm,
-        'messages': messages_to_display
-    })
-
+        if user is not None:
+            token= TokenGeneratorValidator.make_token(user) #Generating hash 
+            tokenID= TokensModel.objects.create(token=token, user_id=user.id) # Registering token to database
+            current_site= get_current_site(request) #Geting the curent site domain
+            mail_subject= 'Password Reset' #Email to be sent preparation process
+            message= render_to_string('auth/mail/passwordResetMail.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': token
+            })
+            to_email= requestEmail
+            email= EmailMessage(
+            mail_subject, message, to=[to_email]
+        )
+            email.send()
+            tokenID.save()
+            messages.success(request, 'Please check your email to reset your password..')
+            return JsonResponse({'msg':'Succes', 'status': 'ok'}, safe= False)
+        else:
+            messages.error(request, 'No account was found with the provided E-mail. Please check and try again...')
+            return JsonResponse({'msg':'No account was found with the provided E-mail. Please check and try again...', 'status': 'not_ok'}, safe= False)
 def passwordResetConfirm(request, uidb64, token):
     user= auth.get_user_model()
 
@@ -438,7 +428,7 @@ def passwordResetConfirm(request, uidb64, token):
     except (TypeError, ValidationError, OverflowError, User.DoesNotExist):
         user= None
     print(token)
-    not_expired = TokenGeneratorValidator.check_token(user, token, settings.PASSWORD_RESET_TOKEN_EXPIRY_DURATION)
+    not_expired = TokenGeneratorValidator.check_token(user, token, settings.PASSWORD_RESET_TOKEN_EXPIRY_DURATION, None)
     if user is not None and not_expired:
         # TokensModel.delete(token= token)
         return redirect('password-change', id=uidb64)
@@ -447,42 +437,34 @@ def passwordResetConfirm(request, uidb64, token):
         return redirect('index')
     
 def passwordChange(request, id):
-    forms= PasswordChangeForm()
     if request.method == 'POST':
-        form= PasswordChangeForm(request.POST)
-        if form.is_valid():
-            email= form.cleaned_data.get('email')
-            passwordOne= form.cleaned_data.get('passwordOne')
-            # passwordTwo= form.cleaned_data.get('passwordTwo')
-            try: 
-                user= User.objects.get(email= email)
-            except (TypeError, ValidationError, User.DoesNotExist):
-                user= None
-            if user is not None and user.id == int(force_str(urlsafe_base64_decode(id))):
-                user.set_password(passwordOne)
-                user.save()
-                TokensModel.objects.get(user_id= user.id).delete()
-                current_site= get_current_site(request) #Geting the curent site domain
-                mail_subject= 'Password Reset Confirmation' #Email to be sent preparation process
-                message= render_to_string('auth/mail/passwordResetDone.html', {
-                    'user': user,
-                    'domain': current_site,
-                })
-                to_email= form.cleaned_data.get('email')
-                email= EmailMessage(
-                mail_subject, message, to=[to_email]
-            )
-                email.send()
-                messages.success(request, 'Your password has been changed successfully')
-                return redirect('index')
-            else:
-                messages.error(request, 'Password reset failed make sure to use the required E-mail and link')
-                return redirect('index')
+        passwordOne= request.POST.get('ps1')
+        try: 
+            user= User.objects.get(id= int(force_str(urlsafe_base64_decode(id))))
+        except (TypeError, ValidationError, User.DoesNotExist):
+            user= None
+        if user is not None:
+            user.set_password(passwordOne)
+            user.save()
+            TokensModel.objects.get(user_id= user.id).delete()
+            current_site= get_current_site(request) #Geting the curent site domain
+            mail_subject= 'Password Reset Confirmation' #Email to be sent preparation process
+            message= render_to_string('auth/mail/passwordResetDone.html', {
+                'user': user,
+                'domain': current_site,
+            })
+            to_email= user.email
+            email= EmailMessage(
+            mail_subject, message, to=[to_email]
+        )
+            email.send()
+            messages.success(request, 'Your password has been changed successfully')
+            return JsonResponse({'msg':'Success'}, safe= False)
         else:
-            messages.error(request, f'{next(iter(form.errors.values()))[0]}')
-            return redirect('index')
+            messages.error(request, 'Password reset failed make sure to use the required E-mail and link')
+            return JsonResponse({'msg':'Failed'}, safe= False)
     else:
-        return render(request, 'auth/newPassword.html', {'form': PasswordChangeForm, 'id': id})
+        return render(request, 'sitepages/passwordChange/index.html', {'id': id})
     
 @login_required(login_url='login')
 def notificationsPage(request):
