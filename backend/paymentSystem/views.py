143b83ad9581af48154cb988c422e13514e3576f
yawanewuoh <judeanewuoh@gmail.com>
2025-01-09 09:13:45 +0000

    from test to live

--- backend/paymentSystem/views.py
+++ backend/paymentSystem/views.py
@@ -16,8 +16,8 @@ from .models import *
 from datetime import datetime
 from adminSystem.models import AdminDeveloperUserModel as developers_account
 
-key= settings.PAYSTACK_SECRET_KEY_TEST
-# key= settings.PAYSTACK_SECRET_KEY_LIVE
+#key= settings.PAYSTACK_SECRET_KEY_TEST
+key= settings.PAYSTACK_SECRET_KEY_LIVE
 # Create your views here.
 
 
@@ -63,7 +63,7 @@ def storePaymentProccess(request):
         carrier_code= request.POST.get('carrier_code')
         carrier_name= request.POST.get('carrier_name')
         # New 
-        if int(amount) == 30:
+        if int(amount) == 1:
             StorePaymentProcess.objects.create(user= request.user, amount= amount, email= email,
                                            carrier_code= carrier_code, carrier_name= carrier_name, contact= contact, payment_type= payment_type).save()
             return JsonResponse({'status': 'ok'}, safe= False)
